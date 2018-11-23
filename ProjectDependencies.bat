3da2f98f1afcde0408da475357be527a0faaaeca
Robot-Fromage <code@clementberthaud.com>
2018-11-23 16:07:49 +0100

    infix: refactoring ProjectDependencies.bat

--- ProjectDependencies.bat
+++ ProjectDependencies.bat
@@ -2,7 +2,7 @@
 setlocal
 pushd %~dp0
 
-set "SCRIPT_PATH=OdysseyTools\OdysseyDependencies\OdysseyDependencies.py"
+set "SCRIPT_PATH=ProjectDependencies.py"
 
 if not exist %SCRIPT_PATH%              goto :no_script
 call python3 -c "import colorama"   ||  goto :python3_error
@@ -10,16 +10,15 @@ call python3 %SCRIPT_PATH% %*       ||  goto :script_error
 goto :EOF
 
 :no_script
-echo Warning: OdysseyDependencies.py not found
+echo Warning: ProjectDependencies.py not found
 goto :EOF
 
 :python3_error
 echo Error: could not find python3.
 echo Make sure it is accessible from environment variables
 echo Make sure required modules are installed
-echo Check Odyssey Developper Documentation on python3 for Odyssey
 goto :EOF
 
 :script_error
-echo Error: OdysseyDependencies.py, found errors
+echo Error: ProjectDependencies.py, found errors
 goto :EOF
